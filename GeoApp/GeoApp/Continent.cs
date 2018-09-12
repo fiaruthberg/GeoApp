daa21a7400cd70a108eb03a29dfbe7aad64b7600
Boris Bumbic <boris.bumbic@gmail.com>
2018-09-12 15:37:06 +0200

    Few changes

--- GeoApp/GeoApp/Continent.cs
+++ GeoApp/GeoApp/Continent.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace GeoApp
 {
-    class Continent
+    public class Continent
     {
 		public int Id { get; set; }
 		public string Name { get; set; }
---
+++ GeoApp/GeoApp/LanguageInCountry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeoApp
+{
+    public class LanguageInCountry
+    {
+        public int LanguageId { get; set; }
+        public Language Language { get; set; }
+
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+    }
+}
