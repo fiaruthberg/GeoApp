ef88646d0713ac04a92045e70438e8e9d6c90c96
Sofia Ruthberg <sofia.ruthberg@gmail.com>
2018-09-13 09:05:28 +0200

    lite metoder

--- GeoApp/GeoApp/Program.cs
+++ GeoApp/GeoApp/Program.cs
@@ -9,7 +9,42 @@ namespace GeoApp
         static void Main(string[] args)
         {
             ClearDatabase();
-            InitDatabase();
+            InitDatabase(); /* BORIS */
+            PageMainMenu(); /* SOFIA */
+        }
+
+        private static void PageMainMenu()
+        {
+            Console.Clear();
+            WriteInWhite("What would you like to do?\n");
+
+            Console.WriteLine("A) Show all countries");
+
+            ConsoleKey command = Console.ReadKey().Key;
+            switch (command)
+            {
+                case ConsoleKey.A: ShowAllCountriesInList() /* REZAN */; break;
+                default: WriteInRed("meh"); Console.ReadKey(); PageMainMenu(); break;
+            }
+        }
+
+        private static void ShowAllCountriesInList()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WriteInWhite(string v)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(v);
+            Console.ResetColor();
+        }
+
+        private static void WriteInRed(string v)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(v);
+            Console.ResetColor();
         }
 
         private static void ClearDatabase()
